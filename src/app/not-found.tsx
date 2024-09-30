3ade392038e7c637b0d6350a06130d436d64f614
Heera Singh Lodhi <heera9331@gmail.com>
2024-10-01 00:40:18 +0530

    basic setup

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,22 +2,22 @@ import "./globals.css";
 import React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function NotFound() {
   return (
-    <div className="min-h-screen min-w-screen flex flex-col items-center justify-start">
-      <div className="flex gap-2">
-        <Button variant={"default"} size={"default"}>
-          <Link href={"/"}>Frontend</Link>
-        </Button>
+    <div className="min-h-screen min-w-screen">
+      <div className="flex flex-col gap-4 items-center mx-auto w-[50%] m-6 p-4">
+        <Image
+          src={"/assets/images/404.svg"}
+          className=""
+          width={250}
+          height={250}
+          alt="404 Error - not found"
+        />
+        <p className="pt-8 pb-2">Something is not found?</p>
         <Button variant={"outline"} size={"default"}>
-          <Link href={"/admin"}>Backend</Link>
-        </Button>
-        <Button variant={"outline"} size={"default"}>
-          <Link href={"/login"}>Login</Link>
-        </Button>
-        <Button variant={"outline"} size={"default"}>
-          <Link href={"/register"}>Register</Link>
+          <Link href={"/"}>Go to home</Link>
         </Button>
       </div>
     </div>
